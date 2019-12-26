ca89eedf2c0d4b084759df3605f653e89a0e8300
Mityaeva Darya Denisovna <d.mityaeva@tcsbank.ru>
2019-12-26 18:18:50 +0300

    Task1-4

--- Task2.Type_Conversion.js
+++ Task2.Type_Conversion.js
@@ -4,7 +4,7 @@ let num = 123;
 let bool = true;
 let typeNull = null;
 let typeUndefined = undefined;
-let obj = {name: "Объект"};
+let obj = {name: "Объект", value: "dfgsdg"};
 typeof(obj)
 
 console.log (`Переменные: 
@@ -13,16 +13,16 @@ console.log (`Переменные:
 \nbool = ${bool}, тип: ${typeof(bool)} 
 \ntypeNull = ${typeNull}, тип: ${typeof(typeNull)}
 \ntypeUndefined = ${typeUndefined}, тип: ${typeof(typeUndefined)}
-\nobj = ${obj}, тип: ${typeof(obj)}`);
+\nobj = ${JSON.stringify(obj)}, тип: ${typeof(obj)}`);
 
 //Строковое преобразование
 console.log (`\nСтроковое преобразование:`);
-console.log (num);
-console.log (str);
-console.log (bool);
+console.log ("" + num); //конкатенация
+console.log ("" + str);
+console.log ('' + bool);
 console.log (String(typeNull));
 console.log (String(typeUndefined));
-console.log (String(obj));
+console.log (String(obj)); 
 //Численное преобразование
 console.log (`\nЧисленное преобразование:`);
 console.log (+num);
